266a6f6fac43cf40ef05edb0809dc503ae6b54f4
gabriel8programmer <gabriel8github@gmail.com>
2023-05-23 21:10:37 -0300

    finish css

--- js/theme.js
+++ js/theme.js
@@ -1,33 +1,21 @@
-
-
 const body = document.querySelector("body")
-
 const btnChangeTheme = document.getElementById("btn-change-theme")
-
 const iconThemes = [...document.querySelectorAll(".icon-theme")]
 
 //functions 
 
 const changeIcon = ()=> {
-
 	iconThemes.map( iconTheme => {
-
 		iconTheme.classList.toggle("invisible")
-
 	})
 }
 
-
 const changeTheme = ()=> {
-
 	//change the theme in body
 	body.classList.toggle("light-theme")
-
 	//change the icons
 	changeIcon()
-
 }
 
 //events
-
 btnChangeTheme.addEventListener("click", changeTheme)
